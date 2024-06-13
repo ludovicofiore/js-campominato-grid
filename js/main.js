52b1c530cdcb9ae2143636729ce276f6a3cccb97
ludoff <ludovicofiore25@gmail.com>
2024-06-13 16:52:17 +0200

    feat: add function to create element, click event  and add element via js

--- js/main.js
+++ js/main.js
@@ -2,3 +2,44 @@
 // Ogni cella ha un numero progressivo, da 1 a 100.
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 // Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+
+
+// creo click event per generare griglia
+
+let myButton = document.querySelector(".play-button");
+
+myButton.addEventListener("click",
+    function() {
+
+        // creo elemento big-square
+        // richiamo contenitore
+        const container = document.querySelector(".square-container");
+
+        // richiamo funzione per creare elemento
+        const bigSquare = createSomething("div", "big-square");
+        console.log(bigSquare);
+
+        // porto elemento in pagina
+        container.append(bigSquare);
+
+        // creo caselle square
+    }
+)
+
+
+
+
+
+// FUNZIONI
+
+// funzione per creare elemento html
+function createSomething (tagName, className) {
+
+    // creo elemento
+    const element = document.createElement(tagName);
+    // creo classe
+    element.classList.add(className);
+
+    return element
+}
+
